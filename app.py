e8176fa72ace35054ce12f669b5d6b1ec6cb0bc6
NotPunchnox <punchnoxpaypal@gmail.com>
2025-01-11 22:00:20 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from src.listen import *
+
+import requests
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+
+text = ""
+
+# Boucle principale
+while len(text) < 1:
+
+    # Traiter l'entrée vocale
+    text = process_audio()
+
+    # Si du texte a été reconnu
+    if text:
+        print(f"\nQuestion reconnue: {text}")
+
+        response = requests.post("http://127.0.0.1:5000/synthesize", json={"text": text})
+
+        if response.status_code == 200:
+            # Sauvegarder temporairement le fichier audio
+            temp_file = "temp_output.wav"
+            with open(temp_file, "wb") as f:
+                f.write(response.content)
+            
+            # Lire l'audio
+            audio = AudioSegment.from_file(temp_file)
+            play(audio)
+            
+            # Supprimer le fichier temporaire
+            os.remove(temp_file)
+            print("Audio lu et supprimé avec succès.")
+        else:
+            print("Erreur :", response.json())
---
+++ test/client-tts.py
@@ -0,0 +1,31 @@
+import requests
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+
+# Installer pydub avec : pip install pydub
+# (et ffmpeg pour lire les fichiers audio)
+
+url = "http://127.0.0.1:5000/synthesize"
+
+while True:
+
+    data = {"text": input("Vous: ")}
+
+    response = requests.post(url, json=data)
+
+    if response.status_code == 200:
+        # Sauvegarder temporairement le fichier audio
+        temp_file = "temp_output.wav"
+        with open(temp_file, "wb") as f:
+            f.write(response.content)
+        
+        # Lire l'audio
+        audio = AudioSegment.from_file(temp_file)
+        play(audio)
+        
+        # Supprimer le fichier temporaire
+        os.remove(temp_file)
+        print("Audio lu et supprimé avec succès.")
+    else:
+        print("Erreur :", response.json())
